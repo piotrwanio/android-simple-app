47d7a433a874c0d838a1325bc2dec32efd5a47b4
piotrwanio <34741162+piotrwanio@users.noreply.github.com>
2018-02-15 16:22:53 +0100

    added BeerExpert class

--- Application/app/src/main/java/com/example/piotr_wanio/application/FindBeerActivity.java
+++ Application/app/src/main/java/com/example/piotr_wanio/application/FindBeerActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
+import java.util.List;
 
 public class FindBeerActivity extends AppCompatActivity {
+    private BeerExpert expert = new BeerExpert();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,11 @@ public class FindBeerActivity extends AppCompatActivity {
         TextView brands = (TextView)findViewById(R.id.brands);
         Spinner color = (Spinner)findViewById(R.id.color);
         String beerType = String.valueOf(color.getSelectedItem());
-        brands.setText(beerType);
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for(String brand:brandsList){
+            brandsFormatted.append(brand).append('\n');
+        }
+        brands.setText(brandsFormatted);
     }
 }
